5696f31eee0bac8d4d9d24ef5021eb6870cb4b10
lee jeongmin <ljm991108@gmail.com>
2023-12-10 22:35:22 +0900

    feat: 카피라이트 표기 수정

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import * as S from './styled';
 
+/**
+ * 이 파일은 삭제/수정하지 말아주세요!
+ */
 const Footer: React.FC = () => {
   return (
     <S.Wrapper>
       <S.Footer>
-        © {new Date().getFullYear()}
-        &nbsp;powered by <S.Link href='https://github.com/danmin20'>danmin</S.Link>
+        © Powered by <S.Link href='https://github.com/danmin20'>danmin</S.Link>
       </S.Footer>
     </S.Wrapper>
   );
